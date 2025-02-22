c67a799c6fc482b9cfb52041b8936f8cb0437f8d
Madhav <mvaliyap@purdue.edu>
2025-02-22 11:38:41 -0500

    Working function to create google calendar event given params

--- calendarTest.py
+++ calendarTest.py
@@ -11,7 +11,7 @@ from googleapiclient.errors import HttpError
 # SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-def create_event(title, location, description, start_date, end_date, guests):
+def create_event(title, description, start_date, end_date, guests, timezone='America/Indianapolis', location=' ', recurrence=[], recurring=' ', amountRecur=' '):
   creds = None
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
@@ -30,6 +30,13 @@ def create_event(title, location, description, start_date, end_date, guests):
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
+    
+  if recurring != ' ':
+    if amountRecur != ' ':
+      recurrence.append(f'RRULE:FREQ={recurring};{amountRecur}')
+    else: 
+      recurrence = [f'RRULE:FREQ={recurring}',]
+    
 
   try:
     service = build("calendar", "v3", credentials=creds)
@@ -39,20 +46,15 @@ def create_event(title, location, description, start_date, end_date, guests):
       'location': location,
       'description': description,
       'start': {
-        'dateTime': '2025-02-21T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
+        'dateTime': start_date,
+        'timeZone': timezone,
       },
       'end': {
-        'dateTime': '2025-02-21T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
+        'dateTime': end_date,
+        'timeZone': timezone,
       },
-      'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-      ],
-      'attendees': [
-        {'email': 'madhavsv05@gmail.com'},
-        {'email': 'vpmadhav@gmail.com'},
-      ],
+      'recurrence': recurrence,
+      'attendees': guests,
       'reminders': {
         'useDefault': False,
         'overrides': [
@@ -68,67 +70,70 @@ def create_event(title, location, description, start_date, end_date, guests):
   except HttpError as error:
     print(f"An error occurred: {error}")
 
-  event = service.events().insert(calendarId='primary', body=event).execute()
-  print('Event created: %s' % (event.get('htmlLink')))
+  # event = service.events().insert(calendarId='primary', body=event).execute()
+  # print('Event created: %s' % (event.get('htmlLink')))
 
-def main():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+# def main():
+#   """Shows basic usage of the Google Calendar API.
+#   Prints the start and name of the next 10 events on the user's calendar.
+#   """
 
-  try:
-    service = build("calendar", "v3", credentials=creds)
+#   create_event(title='Function Test',location='walc',description=' ', start_date='2025-02-20T09:00:00', end_date='2025-02-20T11:00:00',guests=[{'email': 'madhavsv05@gmail.com'},], recurring='WEEKLY', amountRecur='COUNT=3')
+#   # creds = None
+#   # The file token.json stores the user's access and refresh tokens, and is
+#   # created automatically when the authorization flow completes for the first
+#   # time.
 
-    event = {
-      'summary': 'test2',
-      'location': 'corec',
-      'description': 'Test',
-      'start': {
-        'dateTime': '2025-02-20T09:00:00',
-        'timeZone': 'America/Indianapolis',
-      },
-      'end': {
-        'dateTime': '2025-02-20T11:00:00',
-        'timeZone': 'America/Indianapolis',
-      },
-      'recurrence': [
-      ],
-      'attendees': [
-        {'email': 'madhavsv05@gmail.com'},
-        {'email': 'vpmadhav@gmail.com'},
-      ],
-      'reminders': {
-        'useDefault': False,
-        'overrides': [
-          {'method': 'popup', 'minutes': 30},
-        ],
-      },
-    }
+#   # if os.path.exists("token.json"):
+#   #   creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+#   # # If there are no (valid) credentials available, let the user log in.
+#   # if not creds or not creds.valid:
+#   #   if creds and creds.expired and creds.refresh_token:
+#   #     creds.refresh(Request())
+#   #   else:
+#   #     flow = InstalledAppFlow.from_client_secrets_file(
+#   #         "credentials.json", SCOPES
+#   #     )
+#   #     creds = flow.run_local_server(port=0)
+#   #   # Save the credentials for the next run
+#   #   with open("token.json", "w") as token:
+#   #     token.write(creds.to_json())
 
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created: %s' % (event.get('htmlLink')))
+#   # try:
+#   #   service = build("calendar", "v3", credentials=creds)
 
-  except HttpError as error:
-    print(f"An error occurred: {error}")
+#   #   event = {
+#   #     'summary': 'test2',
+#   #     'location': 'corec',
+#   #     'description': 'Test',
+#   #     'start': {
+#   #       'dateTime': '2025-02-20T09:00:00',
+#   #       'timeZone': 'America/Indianapolis',
+#   #     },
+#   #     'end': {
+#   #       'dateTime': '2025-02-20T11:00:00',
+#   #       'timeZone': 'America/Indianapolis',
+#   #     },
+#   #     'recurrence': [
+#   #     ],
+#   #     'attendees': [
+#   #       {'email': 'madhavsv05@gmail.com'},
+#   #       {'email': 'vpmadhav@gmail.com'},
+#   #     ],
+#   #     'reminders': {
+#   #       'useDefault': False,
+#   #       'overrides': [
+#   #         {'method': 'popup', 'minutes': 30},
+#   #       ],
+#   #     },
+#   #   }
+
+#   #   event = service.events().insert(calendarId='primary', body=event).execute()
+#   #   print('Event created: %s' % (event.get('htmlLink')))
+
+#   # except HttpError as error:
+#   #   print(f"An error occurred: {error}")
 
 
-if __name__ == "__main__":
-  main()
+# if __name__ == "__main__":
+#   main()
